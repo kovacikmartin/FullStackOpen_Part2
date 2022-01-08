9feecb2bf9136d80031a64cc05b4fabe648440f4
Martin Kováčik <martn.kovacik@gmail.com>
2022-01-08 17:53:10 +0100

    fixed courseinfo 2.1-2.5

---
+++ courseinfo/src/components/Course.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Course = ({course}) => {
+
+    return(
+      <>
+        <Header course={course}/>
+        <Content course={course} />
+        <Total course={course} />
+      </>
+    )
+  }
+
+  const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+  
+    const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+    
+    return(
+      <p>total of {sum} exercises</p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    
+    return (
+      <div>
+        {course.parts.map(part => 
+          <Part key={part.id} part={part}/>
+        )} 
+      </div>
+    )
+  }
+
+  export default Course
